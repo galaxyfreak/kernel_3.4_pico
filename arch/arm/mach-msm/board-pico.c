d3cf52f06f78f208de5ab084c55cab3eb4514d3d
galaxyfreak <marinspajic17@hotmail.com>
2014-07-20 13:14:59 +0200

    Panel fixes
    - We are using CMD mode
    - init_pico is never set to 1 (also removed in K3.0 DX msm_fb port)

--- arch/arm/mach-msm/board-pico.c
+++ arch/arm/mach-msm/board-pico.c
@@ -2638,7 +2638,6 @@ static void __init msm7x27a_pm_init(void)
 
 /* For Display */
 extern int panel_type;
-static int init_pico = 0;
 
 void mdp_color_enhancement(struct mdp_reg *reg_seq, int size);
 
@@ -2707,8 +2706,7 @@ static int mipi_panel_power(int on)
 
 	mipi_power_save_on = flag_on;
 
-	if (init_pico != 0)
-		pico_panel_power(on);
+	pico_panel_power(on);
 
 	return 0;
 }
